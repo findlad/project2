ebfa0b1e0d06fb3c30bfb0658abae1f2d9e68066
Findlad <findlad@gmail.com>
2023-11-23 11:38:17 -0700

    iot edits

--- iot.js
+++ iot.js
@@ -3,28 +3,23 @@ const delay = (milliseconds) =>
 
 // Function to calculate the mean of an array
 function calculateMean(array) {
-  // Check if the array is not empty
   if (array.length === 0) {
-    return 0; // or you can choose to return NaN or any other value
+    return NaN; // or you can choose to return NaN or any other value
   }
-
   // Calculate the sum of the array elements
   let sum = 0;
   for (let i = 0; i < array.length; i++) {
-    array[i] = Number.array[i];
+    array[i] = Number(array[i]);
     sum += array[i];
   }
-
-  // Calculate the mean
   let mean = sum / array.length;
-
   return mean;
 }
 
 const socket = new WebSocket("ws://homeassistant.local:8123/api/websocket");
 
-socket.onopen = (event) => {
-  //console.log("WebSocket connection opened:", event);
+socket.onopen = async (event) => {
+  console.log("WebSocket connection opened:", event);
 
   // Authenticate with Home Assistant
   socket.send(
@@ -36,12 +31,17 @@ socket.onopen = (event) => {
   );
 
   // Listen to changes sent by IOT
-  // socket.send(
-  //   JSON.stringify({
-  //     id: incrimentalId,
-  //     type: "subscribe_events",
-  //   })
-  // );
+  socket.send(
+    JSON.stringify({
+      id: incrimentalId,
+      type: "subscribe_events",
+    })
+  );
+
+  await delay(500);
+  // Call getCurrentSwitchState immediately when the page loads
+
+  getCurrentState();
 };
 let weightArray = [];
 let switchState;
@@ -49,6 +49,7 @@ let manualToggle = false; // Flag to track manual switch toggles
 //process recieved messages
 socket.onmessage = (event) => {
   try {
+    console.log(event);
     const receivedData = JSON.parse(event.data);
 
     if (
@@ -56,20 +57,20 @@ socket.onmessage = (event) => {
       // && Array.isArray(receivedData.result)
     ) {
       let background = document.getElementById("background");
-      const iotThing = document.getElementById("switch");
+      let iotSwitch = document.getElementById("switch");
 
-      // console.log("receivedData.event");
+      console.log("receivedData.event");
 
       if (receivedData.event.data.entity_id === "switch.thing2") {
-        const switchState = receivedData.event.data.new_state.state === "on";
+        switchState = receivedData.event.data.new_state.state === "on";
 
         if (switchState) {
           //if its on, check the toggleswithc, and add "on" to the body
-          iotThing.checked = true;
+          iotSwitch.checked = true;
           background.classList.remove("on", "off");
           background.classList.add("on");
         } else {
-          iotThing.checked = false; //if its off, check the toggle switch, and add "off" to the body
+          iotSwitch.checked = false; //if its off, check the toggle switch, and add "off" to the body
           background.classList.remove("on", "off");
           background.classList.add("off");
         }
@@ -78,7 +79,7 @@ socket.onmessage = (event) => {
         receivedData.event.data.entity_id ===
         "binary_sensor.presence_sensor_fp2_1708_presence_sensor_1"
       ) {
-        const fall = true;
+        const fall = true; //using the presence sensor as fall detection for now
         console.log("fall detected");
         const fallbox = document.getElementById("fallbox");
         fallbox.innerHTML = "FALL";
@@ -93,43 +94,51 @@ socket.onmessage = (event) => {
 
         const instantWeight = receivedData.event.data.new_state.state;
         console.log(instantWeight);
-        if ((instantWeight = !undefined)) {
+        if (instantWeight > 0) {
           weightArray.push(instantWeight);
           console.log(weightArray);
         }
-        if ((instantWeight = 0)) {
+        if (instantWeight === 0) {
           weightArray.splice(0, 3);
           weightArray.splice(weightArray.length - 3, 3);
           aveWeight = calculateMean(weightArray);
+          console.log("average weight = " + aveWeight);
+          const weightbox = document.getElementById("weightbox");
+          weightbox.innerHTML = "Weight: " + aveWeight;
         }
-        const weightbox = document.getElementById("weightbox");
-        weightbox.innerHTML = "Weight: " + aveWeight;
       }
 
-      // add listener for the other sensors
+      // initial state sensing
 
-      // if (
-      //   receivedData.type === "result" &&
-      //   Array.isArray(receivedData.result)
-      // ) {
-      //   const resultArray = receivedData.result;
-      //   for (let i = 0; i < resultArray.length; i++) {
-      //     let currentEntry = resultArray[i];
-      //     if (
-      //       currentEntry.entity_id ===
-      //       "binary_sensor.presence_sensor_fp2_1708_presence_sensor_1"
-      //     ) {
-      //       //when we find sensor......
-      //       console.log(currentEntry);
-      //       break;
-      //     }
-      //   }
-      // } else {
-      //   console.warn(
-      //     "Received data does not match the expected format:",
-      //     receivedData
-      //   );
-      // }
+      if (
+        receivedData.type === "result" &&
+        Array.isArray(receivedData.result)
+      ) {
+        const resultArray = receivedData.result;
+        console.log(resultArray);
+        for (let i = 0; i < resultArray.length; i++) {
+          let currentEntry = resultArray[i];
+          if (currentEntry.entity_id === "switch.thing2") {
+            switchState = currentEntry.state;
+            const iotThing = document.getElementById("switch");
+            if (switchState === "on") {
+              iotThing.checked = true;
+            } else {
+              iotThing.checked = false;
+            }
+
+            //console.log(switchState);
+            switchContainer.classList.remove("on", "off"); //Remove both classes
+            switchContainer.classList.add(switchState); // Add the current state as a class
+            break;
+          }
+        }
+      } else {
+        console.warn(
+          "Received data does not match the expected format:",
+          receivedData
+        );
+      }
     }
   } catch (error) {
     console.error("Error parsing JSON:", error);
@@ -143,7 +152,7 @@ socket.onclose = (event) => {
 };
 //sending message to the pi
 function sendMessage(message) {
-  //console.log("Sending message:", message);
+  console.log("Sending message:", message);
   socket.send(message);
 }
 // start a counter so theres a different id every time
@@ -156,16 +165,12 @@ function getCurrentState() {
     type: "get_states",
   });
   incrimentalId++;
+  console.log(message);
   sendMessage(message);
 }
 
 // Call getCurrentSwitchState every 2 seconds, unless theres been a manual switch recently
-setInterval(() => {
-  getCurrentState();
-}, 2000);
-
-// Call getCurrentSwitchState immediately when the page loads
-getCurrentState();
+// setInterval(getCurrentState, 1000);
 
 //tell the switch to toggle
 function toggleSwitch() {
