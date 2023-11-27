cb121863a7ef73aab65b03541ddb413724e01cfa
Findlad <findlad@gmail.com>
2023-11-27 14:06:26 -0700

    added background images, fixed time

--- time_script.js
+++ time_script.js
@@ -1,18 +1,32 @@
 // Function to update date and time
 function updateDateTime() {
-  const dateElement = document.getElementById("date");
+  const dateElement = document.getElementById("date_2");
   const timeElement = document.getElementById("time");
+  console.log(dateElement);
 
   // Get the current date and time
   const currentDate = new Date();
-  const optionsDate = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' };
-  const optionsTime = { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
-  const formattedDate = currentDate.toLocaleDateString('en-US', optionsDate);
-  const formattedTime = currentDate.toLocaleTimeString('en-US', optionsTime);
+  const optionsDate = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  };
+  console.log("2nd");
+  const optionsTime = {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: true,
+  };
+  const formattedDate = currentDate.toLocaleDateString("en-US", optionsDate);
+  const formattedTime = currentDate.toLocaleTimeString("en-US", optionsTime);
+  console.log("3rd");
 
   // Update the date and time elements
   dateElement.textContent = formattedDate;
   timeElement.textContent = formattedTime;
+  console.log("4th");
 }
 
 // Call the function to initially set the date and time
