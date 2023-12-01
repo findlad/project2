91b24010c15e46f10641ebaa6c9812a2de8d50e3
Findlad <findlad@gmail.com>
2023-12-01 09:44:32 -0700

    fixed stuff

---
+++ entertainment.js
@@ -0,0 +1,26 @@
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  coverflowEffect: {
+    rotate: 10,
+    stretch: 0,
+    depth: 50,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
