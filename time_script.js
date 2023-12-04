1b5213c0e13749a166b22724a2364266f2b802a6
Rafiq Lightwala <rafiq.lightwala@gmail.com>
2023-12-04 11:19:51 -0700

    Date locale is also language based

--- time_script.js
+++ time_script.js
@@ -1,5 +1,11 @@
-// Function to update date and time
-function updateDateTime() {
+// Function to update date and time with a specified locale
+function updateDateTime(localeStart = "en") {
+  var locale;
+  if (localeStart === "en") {
+    locale = "en-US";
+  } else {
+    locale = "fr-FR";
+  }
   const dateElement = document.getElementById("date");
   const timeElement = document.getElementById("time");
 
@@ -7,27 +13,28 @@ function updateDateTime() {
   const currentDate = new Date();
   const optionsDate = {
     weekday: "long",
-    month: "long",
     day: "numeric",
+    month: "long",
     year: "numeric",
   };
 
   const optionsTime = {
-    hour: "numeric",
-    minute: "numeric",
-    // second: "numeric",
-    hour12: true,
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: locale === "en-US", // Use 12-hour format for en-US, 24-hour for fr-FR
   };
-  const formattedDate = currentDate.toLocaleDateString("en-US", optionsDate);
-  const formattedTime = currentDate.toLocaleTimeString("en-US", optionsTime);
+
+  const formattedDate = currentDate.toLocaleDateString(locale, optionsDate);
+  const formattedTime = currentDate.toLocaleTimeString(locale, optionsTime);
 
   // Update the date and time elements
   dateElement.textContent = formattedDate;
   timeElement.textContent = formattedTime;
 }
 
-// Call the function to initially set the date and time
-updateDateTime();
+// Call the function with desired locale
+updateDateTime("en"); // for English (US)
+// updateDateTime("fr-FR"); // for French (France)
 
 // Update the date and time every second (you can adjust the interval)
-setInterval(updateDateTime, 1000);
+setInterval(() => updateDateTime(localStorage.getItem('language')), 1000); // Change the locale as needed
