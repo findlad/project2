77369df8e7df598d54bbc7bed57ac0aa6eea92a5
Findlad <findlad@gmail.com>
2023-12-06 11:02:49 -0700

    fixed iot swiper

--- carousel.js
+++ carousel.js
@@ -1,25 +1,32 @@
-var TrandingSlider = new Swiper(".tranding-slider", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  loop: true,
-  slidesPerView: "auto",
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-  coverflowEffect: {
-    rotate: 10,
-    stretch: 100,
-    depth: 70,
-    modifier: 2.5,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+console.log("Start of js file");
+try {
+  var TrandingSlider = new Swiper(".tranding-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: "auto",
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    coverflowEffect: {
+      rotate: 10,
+      stretch: 100,
+      depth: 70,
+      modifier: 2.5,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+} catch (error) {
+  console.log("TrandingSlider Crashed!");
+  console.log(error);
+}
+console.log("End of js file");
