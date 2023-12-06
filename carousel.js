859dc06f65edc96574502bde873f4b818be7c7da
Findlad <findlad@gmail.com>
2023-12-06 10:35:24 -0700

    cosmetic fixes

--- carousel.js
+++ carousel.js
@@ -1,26 +1,25 @@
-var TrandingSlider = new Swiper('.tranding-slider', {
-  effect: 'coverflow',
+var TrandingSlider = new Swiper(".tranding-slider", {
+  effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
   loop: true,
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   autoplay: {
     delay: 5000,
     disableOnInteraction: false,
   },
   coverflowEffect: {
     rotate: 10,
-    stretch: 0,
-    depth: 50,
+    stretch: 100,
+    depth: 70,
     modifier: 2.5,
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
-
